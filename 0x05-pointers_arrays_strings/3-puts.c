06c8e71e1d19ae0398a43d89209bbc1c4b82e8d1
dotun12 <adesanyaadedotun3@gmail.com>
2023-08-22 11:40:03 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-/**
- * _puts - function that prints a string
- * @str: string to be printed
+/*
+ * _puts - prints a string
+ * @str: the spring to print
  * Return: void
  */
 
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-		_putchar(str[i]);
+	for (; *str != '\0' ; str++)
+	{
+		_putchar(*str);
+	}
 	_putchar('\n');
 }
