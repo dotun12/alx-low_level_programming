0f5f8cfeec84039444902d1db970934f70b7cd1c
dotun12 <adesanyaadedotun3@gmail.com>
2023-08-28 11:29:08 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,25 +1,14 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * print_diagsums - Entry point
- * @a: input
- * @size: input
- * Return: Always 0 (Success)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void print_diagsums(int *a, int size)
+int _putchar(char c)
 {
-	int sum1, sum2, y;
-
-	sum1 = 0;
-	sum2 = 0;
-
-	for (y = 0; y < size; y++)
-	{
-		sum1 = sum1 + a[y * size + y];
-	}
-	for (y = size - 1; y >= 0; y--)
-	{
-		sum2 += a[y * size + (size - y - 1)];
-	}
-	printf("%d, %d\n", sum1, sum2);
+	return (write(1, &c, 1));
 }
